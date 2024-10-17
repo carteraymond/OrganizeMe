a4e7a2e9c423bb1e3e75cf1e9044f87937b93666
JohnnyASan <johnanthonysanabria@gmail.com>
2024-10-17 14:32:03 -0600

    added a controller to create user

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../api-docs/swagger-output.json';
 import path from 'path';
+import { createUser } from '../controllers/userController';
+
 const router = express.Router();
 
 router.get('/api-docs', swaggerUi.serve);
@@ -11,5 +13,7 @@ const __dirname = path.resolve(path.dirname(''));
 router.get('/', (req: Request, res: Response): void => {
     res.sendFile(path.join(__dirname, '../dist/organize-me/browser/index.html'));
 });
+
+router.post('/user', createUser);
 
 export default router;
