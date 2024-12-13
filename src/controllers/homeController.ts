b0fea3fc55ebe0cb0e2dbf5b496692633531e1bb
carteraymond <craymond78@gmail.com>
2024-12-12 22:04:09 -0500

    lets go inside our house

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 import { AuthRequest } from './authController';
 
 async function loadHomePage(req: AuthRequest, res: Response): Promise<void> {
+  console.log(`session Log: ${JSON.stringify(req.session.user)} Also, this is inside LoadHomePage`);
   // Security check to prevent unauthorized access to home page
     if (!req.session.user) {
       console.log('User not authenticated');
